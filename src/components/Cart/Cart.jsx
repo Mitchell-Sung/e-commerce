576910381ae77204a84dfa16aebf1f927f3143e6
Mitchell-Sung <mitchell.sung@gmail.com>
2021-06-26 02:58:50 -0600

    Set up cart buttons

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,55 +4,67 @@ import useStyles from './cart.styles';
 import CartItem from '../CartItem/CartItem';
 import { Container, Typography, Button, Grid } from '@material-ui/core';
 
-const Cart = ({ cart }) => {
-	console.log('cart :>> ', cart);
+const Cart = ({
+	cart,
+	handleUpdateCartQty,
+	handleRemoveFromCart,
+	handleEmptyCart,
+}) => {
 	const classes = useStyles();
 
 	const EmptyCart = () => {
-		<Typography variant='subtitle1'>
-			You have no items in your shopping cart,
-			<Link to='/' className={classes.link}>
-				Start adding some!
-			</Link>
-			!
-		</Typography>;
+		return (
+			<Typography variant='subtitle1'>
+				You have no items in your shopping cart,
+				<Link to='/' className={classes.link}>
+					Start adding some!
+				</Link>
+			</Typography>
+		);
 	};
 
 	const FilledCart = () => {
-		<Fragment>
-			<Grid container spacing={3}>
-				{cart.line_items.map((item) => (
-					<Grid item xs={12} sm={4} key={item.id}>
-						<CartItem item={item} />
-					</Grid>
-				))}
-			</Grid>
-			<div className={classes.cardDetails}>
-				<Typography variant='h4'>
-					Subtotal: {cart.subtotal.formatted_with_symbol}
-				</Typography>
-				<div>
-					<Button
-						className={classes.emptyButton}
-						size='large'
-						type='button'
-						variant='contained'
-						color='secondary'
-					>
-						Empty Cart
-					</Button>
-					<Button
-						className={classes.checkoutButton}
-						size='large'
-						type='button'
-						variant='contained'
-						color='secondary'
-					>
-						Checkout
-					</Button>
+		return (
+			<Fragment>
+				<Grid container spacing={3}>
+					{cart.line_items.map((item) => (
+						<Grid item xs={12} sm={4} key={item.id}>
+							<CartItem
+								item={item}
+								onUpdateCartQty={handleUpdateCartQty}
+								onRemoveFromCart={handleRemoveFromCart}
+							/>
+						</Grid>
+					))}
+				</Grid>
+				<div className={classes.cardDetails}>
+					<Typography variant='h4'>
+						Subtotal: {cart.subtotal.formatted_with_symbol}
+					</Typography>
+					<div>
+						<Button
+							className={classes.emptyButton}
+							size='large'
+							type='button'
+							variant='contained'
+							color='secondary'
+							onClick={handleEmptyCart}
+						>
+							Empty Cart
+						</Button>
+						<Button
+							className={classes.checkoutButton}
+							size='large'
+							type='button'
+							variant='contained'
+							color='secondary'
+						>
+							Checkout
+						</Button>
+					</div>
 				</div>
-			</div>
-		</Fragment>;
+			</Fragment>
+		);
 	};
 
 	if (!cart.line_items) return 'Loading';
