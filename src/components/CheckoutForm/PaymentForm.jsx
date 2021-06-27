c20ab7c89dd2f51ca05b51abeccf219b674e2ca0
Mitchell-Sung <mitchell.sung@gmail.com>
2021-06-26 20:07:19 -0600

    Set up review component

--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Review from '../../components/Review/Review';
+import { Typography, Button, Divider } from '@material-ui/core';
+import { loadStripe } from '@stripe/stripe-js';
+import {
+	Elements,
+	CardElement,
+	ElementsConsumer,
+} from '@stripe/react-stripe-js';
 
-const PaymentForm = () => {
-	return <div>PaymentForm</div>;
+const PaymentForm = ({ checkoutToken }) => {
+	return (
+		<Fragment>
+			<Review checkoutToken={checkoutToken} />
+		</Fragment>
+	);
 };
 
 export default PaymentForm;
